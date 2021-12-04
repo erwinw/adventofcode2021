98b80a59f913ae9c4623053c855c1c9eb1e6c58b
Erwin Wessels <erwin@pleo.io>
2021-12-04 21:06:50 +0100

    Day 04 A

---
+++ main/src/com/transpontine/aoc2021/day04/Board.kt
@@ -0,0 +1,62 @@
+package com.transpontine.aoc2021.day04
+
+class Board(lines: List<String>) {
+    private val cells = lines.also {
+        println("LINES:")
+        it.forEachIndexed { index, l -> println(">> [$index] $l") }
+    }
+        .map { line ->
+            line
+                .trim()
+                .split(Regex("\\s+"))
+                .map { num ->
+                    Cell(num.toByte())
+                }
+        }
+
+    fun pickChip(chip: Byte) {
+        cells.forEach { row ->
+            row.forEach { cell ->
+                if (cell.number == chip) {
+                    cell.taken = true
+                }
+            }
+        }
+    }
+
+    val hasBingo: Boolean
+        get() {
+            cells.forEach { row ->
+                if (row.all { it.taken }) {
+                    return true
+                }
+            }
+            for (colIdx in 0 until cells[0].size) {
+                if (cells.all { it[colIdx].taken }) {
+                    return true
+                }
+            }
+
+            return false
+        }
+
+    fun sumLeft() =
+        cells.fold(0) { accRow, row ->
+            accRow + row
+                .filter { !it.taken }
+                .fold(0) { accCell, cell -> accCell + cell.number.toInt() }
+        }
+
+    override fun toString(): String {
+        return cells.joinToString("\n") { row ->
+            row
+                .joinToString(" ") { cell ->
+                    if (cell.taken) {
+                        "[%2s]".format(cell.number)
+                    } else {
+                        " %2s ".format(cell.number)
+                    }
+                }
+        } + "\n"
+    }
+}
---
+++ main/src/com/transpontine/aoc2021/day04/Cell.kt
@@ -0,0 +1,5 @@
+package com.transpontine.aoc2021.day04
+
+class Cell(val number: Byte) {
+    var taken: Boolean = false
+}
